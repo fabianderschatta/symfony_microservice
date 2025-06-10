f93d475192bb45646c2a2955e72c9ec23301c9ab
Fabian Derschatta <fabian@derschatta.de>
2025-06-10 20:23:09 +1200

    Add JWT package

--- user/config/bundles.php
+++ user/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
